6ba65b517fd8c9da95c5dd73df05f550f22e1390
MarlonChi <marlonchiodelli@hotmail.com>
2025-08-21 21:50:37 -0300

    chore: firebase

--- firebase/client.ts
+++ firebase/client.ts
@@ -2,15 +2,7 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyANgQwJSRj-D9yVQ2exJzE938vW04gGTFA",
-  authDomain: "speek-rate.firebaseapp.com",
-  projectId: "speek-rate",
-  storageBucket: "speek-rate.firebasestorage.app",
-  messagingSenderId: "187030355920",
-  appId: "1:187030355920:web:294e8d0695e9ccc4b843d5",
-  measurementId: "G-KHGCLZQDJG",
-};
+const firebaseConfig = {};
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
